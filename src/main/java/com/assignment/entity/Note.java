27e043e2f309d0fe2a29ba0077d7c0990a554f95
HoangHai <hoanghai.yps@gmail.com>
2020-06-10 19:39:25 +0700

    config hibernate

---
+++ src/main/java/com/assignment/entity/Note.java
@@ -0,0 +1,58 @@
+package com.assignment.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "note")
+public class Note {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    private java.sql.Date createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public java.sql.Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(java.sql.Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
