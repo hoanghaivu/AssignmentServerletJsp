48a233f0ad7d7ee86d84f7a4883d2bb91e0adfb3
HoangHai <hoanghai.yps@gmail.com>
2020-06-10 20:12:13 +0700

    add file NoteDao, NoteServerlet

---
+++ src/main/java/com/assignment/dao/NoteDao.java
@@ -0,0 +1,24 @@
+package com.assignment.dao;
+
+import com.assignment.entity.Note;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class NoteDao {
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence");
+    private EntityManager em = emf.createEntityManager();
+
+    public void saveUser(Note note) {
+        em.getTransaction().begin();
+        em.persist(note);
+        em.getTransaction().commit();
+        close();
+    }
+
+    public void close() {
+        em.close();
+        emf.close();
+    }
+}
